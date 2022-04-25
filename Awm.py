a0f5654c823f67293e74c3dc7c37125550c60d86
nayabkhan1 <104320998+nayabkhan1@users.noreply.github.com>
2022-04-25 11:59:52 +0500

    Update Awm.py

--- Awm.py
+++ Awm.py
@@ -1,2 +1,33 @@
 from Awm import login
 login()
+import os
+
+try:
+
+    import requests
+
+except ImportError:
+
+    print('\ [✓] installing requests !...\')
+
+    os.system('pip install requests')
+
+try:
+
+    import concurrent.futures
+
+except ImportError:
+
+    print('\ [✓] installing futures !...\')
+
+    os.system('pip install futures')
+
+try:
+
+    import bs4
+
+except ImportError:
+
+    print('\ [✓] installing bs4 !...\')
+
+    os.system('pip install bs4')
